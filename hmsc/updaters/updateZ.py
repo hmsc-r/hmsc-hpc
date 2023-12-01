ad69344263b0064de018f5e46e013e04285b8eb2
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-12-01 08:50:13 +0200

    Remove commented code

--- hmsc/updaters/updateZ.py
+++ hmsc/updaters/updateZ.py
@@ -2,11 +2,11 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_probability as tfp
 from tensorflow.python.ops.random_ops import parameterized_truncated_normal
-# from polyagamma import random_polyagamma
 from scipy.stats import truncnorm
 from hmsc.utils.tf_named_func import tf_named_func
 tfm, tfr = tf.math, tf.random
 tfd = tfp.distributions
+
 
 @tf_named_func("z")
 def updateZ(params, data, rLHyperparams, *,
@@ -15,7 +15,7 @@ def updateZ(params, data, rLHyperparams, *,
             seed=None):
     """Update conditional updater(s)
     Z - latent variable.
-        
+
     Parameters
     ----------
     params : dict
@@ -46,7 +46,7 @@ def updateZ(params, data, rLHyperparams, *,
     distr = data["distr"]
     ny, ns = Y.shape
     nr = len(EtaList)
-    
+
     if len(X.shape.as_list()) == 2: #tf.rank(X)
       LFix = tf.matmul(X, Beta)
     else:
@@ -69,7 +69,6 @@ def updateZ(params, data, rLHyperparams, *,
     ZNormal = tf.where(YoN, YN, LN + sigmaN * tfr.normal([ny, tf.size(indColNormal)], dtype=dtype))
     iDNormal = tf.cast(YoN, dtype) * sigmaN**dtype(-2)
 
-
     # Albert and Chib (1993) data augemntation for probit model in columns with binary data
     indColProbit = np.nonzero(distr[:,0] == 2)[0]
     YP = tf.gather(Y, indColProbit, axis=-1)
@@ -77,15 +76,10 @@ def updateZ(params, data, rLHyperparams, *,
     YmP = tfm.logical_not(YoP)
     LP = tf.gather(L, indColProbit, axis=-1)
     sigmaP = tf.gather(sigma, indColProbit)
-    # low = tf.where(tfm.logical_or(YP == 0, YmP), tf.cast(-np.inf, dtype), tf.zeros_like(YP))
-    # high = tf.where(tfm.logical_or(YP == 1, YmP), tf.cast(np.inf, dtype), tf.zeros_like(YP))
-    # norm = tfd.Normal(LP, sigmaP)
-    # samUnif = tf.random.uniform(YP.shape, norm.cdf(low), norm.cdf(high), dtype=dtype)
-    # ZP = norm.quantile(samUnif)
     low = tf.where(tfm.logical_or(YP == 0, YmP), tf.cast(-INFTY, dtype), tf.zeros_like(YP))
     high = tf.where(tfm.logical_or(YP == 1, YmP), tf.cast(INFTY, dtype), tf.zeros_like(YP))
     nsP = tf.size(indColProbit)
-    
+
     if truncated_normal_library == "tfd":
       ZProbit = tfd.TruncatedNormal(loc=LP, scale=sigmaP, low=low, high=high).sample(name="z-ZProbit")
     elif truncated_normal_library == "tf":
@@ -100,33 +94,8 @@ def updateZ(params, data, rLHyperparams, *,
       loc, scale = tf.reshape(LP,[ny*nsP]), tf.tile(sigmaP,[ny])
       a, b = (tf.reshape(low,[ny*nsP]) - loc) / scale, (tf.reshape(high,[ny*nsP]) - loc) / scale
       ZProbit = tf.reshape(tf.numpy_function(truncnorm.rvs, [a, b, loc, scale], dtype), [ny,nsP])
-    
+
     iDProbit = tf.cast(YoP, dtype) * sigmaP**dtype(-2)
-    
-    
-    # # Lognormal Poisson with 1D slice sampling
-    # indColPoisson = np.nonzero(distr[:,0] == 3)[0]
-    # YPo = tf.gather(Y, indColPoisson, axis=-1)
-    # YoPo = tf.gather(Yo, indColPoisson, axis=-1)
-    # LPo = tf.gather(L, indColPoisson, axis=-1)
-    # ZPrevPo = tf.gather(ZPrev, indColPoisson, axis=-1)
-    # sigmaPo = tf.gather(sigma, indColPoisson)
-    # logLike = lambda a: tfm.multiply_no_nan(tfd.Poisson(log_rate=a).log_prob(YPo), tf.cast(YoPo,dtype)) + tfd.Normal(LPo, sigmaPo).log_prob(a)
-    # sampler = tfp.mcmc.SliceSampler(logLike, step_size=dtype(1e0), max_doublings=10)
-    # kernel_results = sampler.bootstrap_results(ZPrevPo)
-    # ZPoisson, kernel_results_new = sampler.one_step(ZPrevPo, kernel_results)
-    
-    # # Poisson with external PG sampler
-    # indColPoisson = np.nonzero(distr[:,0] == 3)[0]
-    # YPo = tf.gather(Y, indColPoisson, axis=-1)
-    # YoPo = tf.gather(Yo, indColPoisson, axis=-1)
-    # LPo = tf.gather(L, indColPoisson, axis=-1)
-    # pg_h = tf.reshape(YPo, [-1]) + r
-    # pg_z = tf.reshape(LPo, [-1]) - np.log(r) # sign does not matter
-    # draw_pg = lambda h,z: random_polyagamma(h, z, disable_checks=True)
-    # omega = tf.numpy_function(draw_pg, [pg_h, pg_z], dtype)
-    # iDPoisson = tf.cast(YoPo, dtype) * tf.reshape(omega, YPo.shape)
-    # ZPoisson = tf.reshape((0.5*(tf.reshape(YPo, [-1]) - r)) / omega + np.log(r), YPo.shape)
 
     # Lognormal Poisson with external PG sampler
     r = 1000 #Neg-binomial approximation constant
@@ -135,7 +104,7 @@ def updateZ(params, data, rLHyperparams, *,
     YoPo = tf.gather(Yo, indColPoisson, axis=-1)
     LPo = tf.gather(L, indColPoisson, axis=-1)
     sigmaPo = tf.gather(sigma, indColPoisson)
-        
+
     if poisson_preupdate_z == False:
       ZPo = tf.gather(ZPrev, indColPoisson, axis=-1)
     else:
@@ -143,7 +112,7 @@ def updateZ(params, data, rLHyperparams, *,
       sigmaZ2 = (sigmaPo**-2. * tf.ones_like(LPo) + omega)**-1.
       muZ = sigmaZ2*((YPo-r)/2. + omega*np.log(r) + sigmaPo**-2. * LPo)
       ZPo = tfr.normal(YPo.shape, muZ, tf.sqrt(sigmaZ2), dtype=dtype)
-    
+
     omega = draw_polya_gamma(YPo + r, ZPo - np.log(r), dtype=dtype)
     if poisson_marginalize_z == False:
       # sample Z. Required for sigma.
@@ -156,27 +125,20 @@ def updateZ(params, data, rLHyperparams, *,
       iDPoisson = tf.cast(YoPo, dtype) * (sigmaPo**2. * tf.ones_like(LPo) + omega**-1)**-1
       ZPoisson = (YPo-r)/(2.*omega) + np.log(r)
     poisson_omega = omega
-    
 
     ZStack = tf.concat([ZNormal, ZProbit, ZPoisson], -1)
     iDStack = tf.concat([iDNormal, iDProbit, iDPoisson], -1)
     indColStack = tf.concat([indColNormal, indColProbit, indColPoisson], 0)
-    # ZStack = tf.concat([ZNormal, ZProbit], -1)
-    # iDStack = tf.concat([iDNormal, iDProbit], -1)
-    # indColStack = tf.concat([indColNormal, indColProbit], 0)
     ZNew = tf.gather(ZStack, tf.argsort(indColStack), axis=-1)
     iDNew = tf.gather(iDStack, tf.argsort(indColStack), axis=-1)
     return ZNew, iDNew, poisson_omega
-    # return ZNew, iDNew, tf.zeros([0])
-
-
 
 
 def draw_polya_gamma(h, z, dtype=np.float64):
   # with h > 50 normal approx is used, so we reimplement only that alternative
   # pg_h = tf.reshape(h, [-1])
   # pg_z = tf.reshape(z, [-1]) # sign does not matter
-  # draw_pg = lambda h,z: random_polyagamma(h, z, disable_checks=True) 
+  # draw_pg = lambda h,z: random_polyagamma(h, z, disable_checks=True)
   # omega = tf.reshape(tf.numpy_function(draw_pg, [pg_h, pg_z], dtype), h.shape)
   m0 = 0.25 * h
   s0 = tf.sqrt(h / 24.)
