cdc45c30c85c951013a31976be5ac1a5e7253b21
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2024-08-30 11:02:49 +0300

    Explicitely set shape in Psi updater
    At least in spatial model with a single species the previous version of code was failing with Psi shape changing [None,None] instead of [None,1].

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hmsc",
-    version="0.1.0",
+    version="0.1.1",
     author="[removed for review]",
     license='GPLv3+',
     install_requires=[
