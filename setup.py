e6c5e1716d68c54c3d9077647eea9f519bc85861
Tuomas Rossi <tuomas.rossi@csc.fi>
2024-09-09 12:56:10 +0300

    Fix package discovery

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="hmsc",
     version="0.1.1",
     author="[removed for review]",
     license='GPLv3+',
+    packages=find_packages(include=['hmsc', 'hmsc.*']),
     install_requires=[
         'numpy',
         'pandas',
