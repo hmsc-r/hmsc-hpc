63bdb3c0e87562b30592b8a9551fba7c48d579bf
Mihkel Tiks <tiks.mihkel@proton.me>
2024-10-31 17:45:13 +0200

    Use batched version

--- custom_ops/magma_cholesky.cu.cc
+++ custom_ops/magma_cholesky.cu.cc
@@ -7,7 +7,6 @@
 #include <stdexcept>
 #include <iostream>
 #include <cmath>
-#include <chrono>
 using namespace tensorflow;
 
 using GPUDevice = Eigen::GpuDevice;
@@ -16,32 +15,47 @@ template <typename T>
 void MagmaCholeskyFunctor<GPUDevice, T>::operator()(
     const GPUDevice& d, int n, const T* in, T* out, int num_matrices) {
   magma_init();
-  
-  int matrix_size = n*n;
+
+  int matrix_size = n * n;
   hipStream_t stream = NULL;
-  magma_int_t* d_info = new magma_int_t[num_matrices];
+  magma_int_t* d_info;
   magma_device_t device;
   magma_queue_t magma_queue;
-  
+  T** dA_array;
 
-  hipStreamCreateWithFlags(&stream, hipStreamNonBlocking); //Data transfer stream
+  hipStreamCreateWithFlags(&stream, hipStreamNonBlocking); // Data transfer stream
   magma_getdevice(&device);
-  magma_queue_create_from_hip(device,stream, NULL, NULL, &magma_queue); //Magma transpose queue
-  
+  magma_queue_create_from_hip(device, stream, NULL, NULL, &magma_queue); // Magma queue
 
-  hipMalloc(reinterpret_cast<void **>(&d_info), sizeof(int) * num_matrices);
-  hipMemcpy(out, in, sizeof(T) * matrix_size*num_matrices, hipMemcpyDeviceToDevice);
-  hipDeviceSynchronize();
-  
-  
+  // Allocate device memory for d_info and dA_array
+  hipMalloc((void**)&d_info, sizeof(magma_int_t) * num_matrices);
+  hipMalloc((void**)&dA_array, sizeof(T*) * num_matrices);
+
+  // Allocate and copy matrix pointers to dA_array
+  T* matrices = out; // Assuming out points to the batched matrix array
+  T* hA_array[num_matrices];
   for (int i = 0; i < num_matrices; i++) {
-    // Iterate over matrices, calculate cholesky and transpose
-    T* matrix_out = out + i * matrix_size;
-    magma_dpotrf_expert_gpu(MagmaLower, n, matrix_out, n, d_info + i, 1024, MagmaNative);
-    magmablas_dtranspose_inplace(n, matrix_out, n, magma_queue);
+    hA_array[i] = matrices + i * matrix_size;
   }
-  
-  // Copy info
+
+  // Copy matrix pointers to device
+  hipMemcpy(out, in, sizeof(T) * matrix_size * num_matrices, hipMemcpyDeviceToDevice);
+  hipMemcpy(dA_array, hA_array, sizeof(T*) * num_matrices, hipMemcpyHostToDevice);
+  hipDeviceSynchronize();
+
+  if (num_matrices == 1) {
+    // Single matrix case
+    magma_dpotrf_expert_gpu(MagmaLower, n, out, n, d_info, 1024, MagmaNative);
+    magmablas_dtranspose_inplace(n, out, n, magma_queue);
+  } else {
+    // Batched case for multiple matrices
+    magma_dpotrf_batched(MagmaLower, n, dA_array, n, d_info, num_matrices, magma_queue);
+    for (int i = 0; i < num_matrices; i++) {
+      magmablas_dtranspose_inplace(n, hA_array[i], n, magma_queue);
+    }
+  }
+
+  // Copy info back to host
   int* h_info = new int[num_matrices];
   hipMemcpyAsync(h_info, d_info, sizeof(int) * num_matrices, hipMemcpyDeviceToHost, stream);
   hipStreamSynchronize(stream);
@@ -51,14 +65,17 @@ void MagmaCholeskyFunctor<GPUDevice, T>::operator()(
       std::stringstream ss;
       ss << -h_info[i] << "-th parameter wrong for matrix " << i;
       delete[] h_info;
-      hipFree(&d_info);
+      hipFree(d_info);
+      hipFree(dA_array);
       hipStreamDestroy(stream);
+      magma_queue_destroy(magma_queue);
       throw std::runtime_error(ss.str());
     }
   }
 
   delete[] h_info;
-  hipFree(&d_info);
+  hipFree(d_info);
+  hipFree(dA_array);
   hipStreamDestroy(stream);
   magma_queue_destroy(magma_queue);
 };
