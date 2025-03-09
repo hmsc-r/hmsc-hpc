07f3e337aa285a197907deb53be1c770b8e3fb71
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2025-03-09 23:29:35 +0200

    fixed setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(
     name="hmsc",
     version="0.3.0",
     author="[removed for review]",
     license='GPLv3+',
+    packages=find_packages(include=['hmsc', 'hmsc.*']),
     install_requires=[
         'numpy',
         'pandas',
