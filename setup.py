65d37d9ec8be98153137b03e90ccdbf1a059f35f
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2025-04-09 11:16:08 +0300

    Increment patch version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hmsc",
-    version="0.1.1",
+    version="0.1.2",
     author="[removed for review]",
     license='GPLv3+',
     packages=find_packages(include=['hmsc', 'hmsc.*']),
