b84541e46b2586b207b2bea09a268266bbc7af68
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2026-05-02 18:25:50 +0300

    Update requirements and setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     license="GPLv3+",
     packages=find_packages(include=["hmsc", "hmsc.*"]),
     install_requires=[
-        "numpy",
-        "pandas",
+        "numpy>=2.0.0",
+        "pandas>=2.1.0",
         "rdata>=1.0.0",
-        "scipy",
-        "tensorflow",
-        "tensorflow-probability[tf]",
+        "scipy>=1.11.0",
+        "tensorflow>=2.16.0",
+        "tensorflow-probability[tf]>=0.24.0",
     ],
 )
