0b7ba8edcf85df8fafc72c977358a644c1de4380
Gleb Tikhonov <gleb.tikhonov@helsinki.fi>
2026-05-02 17:40:39 +0300

    Update version in setup.py to 0.1.9.dev1+io

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="hmsc",
-    version="0.1.8",
+    version="0.1.9.dev1+io",
     author="[removed for review]",
-    license='GPLv3+',
-    packages=find_packages(include=['hmsc', 'hmsc.*']),
+    license="GPLv3+",
+    packages=find_packages(include=["hmsc", "hmsc.*"]),
     install_requires=[
-        'numpy',
-        'pandas',
-        'rdata>=1.0.0',
-        'scipy',
-        'tensorflow',
-        'tensorflow-probability[tf]',
-    ]
+        "numpy",
+        "pandas",
+        "rdata>=1.0.0",
+        "scipy",
+        "tensorflow",
+        "tensorflow-probability[tf]",
+    ],
 )
